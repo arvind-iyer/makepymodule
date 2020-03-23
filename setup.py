904db8b029f8dc4f5e23357e9e85c1425bc94d02
Arvind Iyer <arvindi.me@gmail.com>
2020-03-23 17:17:53 +0530

    Update version (1.0.1)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    version="1.0.0",
+    version="1.0.1",
     name="makepymodule",
     package_dir={"": "src"},
     package_data={"": ["data/*"]},

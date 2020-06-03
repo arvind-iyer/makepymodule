89609de120d1188df61a9f59420cbc1ec98315a6
Arvind Iyer <arvind.iyer@nokia.com>
2020-06-03 09:47:43 +0530

    update module metadata

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    readme = f.read()
+
 setup(
-    version="1.0.2",
+    version="1.0.3",
     name="makepymodule",
+    author="Arvind Iyer",
+    author_email="arvindi.me@gmail.com",
+    description="CLI program to generate python module directory structure",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arvind-iyer/makepymodule",
     package_dir={"": "src"},
     package_data={"": ["data/*"]},
     packages=find_packages(where="src"),
@@ -12,4 +21,11 @@ setup(
             'makepymodule = makepymodule.cli:main',
         ]
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: POSIX"
+    ]
 )
